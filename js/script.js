1df65055afeeade9216203e07dec0ea0288b58ee
Siddhi Air <siddhiair8@gmail.com>
2020-10-22 00:22:18 +0530

    home main content added

--- js/script.js
+++ js/script.js
@@ -16,5 +16,29 @@ jQuery(document).ready(function($){
 			}
 		]
 	});
+	$(".slider-show-3").slick({
+		dots: false,
+		arrows:true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1179,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
 	
 });
