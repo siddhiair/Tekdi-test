df6a226fc183ad3578b6738214bfcf1905e318a5
Siddhi Air <siddhiair8@gmail.com>
2020-10-22 20:42:44 +0530

    course detail page header and overview-resource tabs done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 jQuery(document).ready(function($){
-	$(".slider-show-2").slick({
-		dots: false,
-		arrows:true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		responsive: [
-			{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-			}
-		]
+	$(".acc-title").click(function(){
+		$(".acc-title").not(this).removeClass("active");
+		$(".acc-body").not($(this).next()).slideUp();
+		$(this).toggleClass("active");
+		$(this).next(".acc-body").slideToggle();
 	});
-	$(".slider-show-3").slick({
+
+	$(".cd-tabs a").click(function(){
+		$(".cd-tabs a").not(this).removeClass("active");
+		$(this).addClass("active");
+		$(".cd-wrapper").hide();
+		var div = $(this).data("target");
+		$("#"+div).fadeIn();
+	});
+	$(".slider-show-2").slick({
 		dots: false,
 		arrows:true,
 		infinite: false,
@@ -24,6 +21,37 @@ jQuery(document).ready(function($){
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		responsive: [
+		{
+			breakpoint: 1599,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+	$(".slider-show-3").slick({
+		dots: false,
+		arrows:true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1599,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
 		{
 			breakpoint: 1179,
 			settings: {
